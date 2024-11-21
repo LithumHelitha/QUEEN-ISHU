01e98bcd0f6c437e1abbd25f3bd5f6a333a7f0d0
LAKSIDU_NIMSARA <laksidunimsara200803041@gmail.com>
2024-11-21 22:06:10 +0530

    Update menu (1).js

--- plugins/menu (1).js
+++ plugins/menu (1).js
@@ -2,201 +2,278 @@ const config = require('../config')
 const {cmd , commands} = require('../command')
 const os = require("os")
 const {runtime} = require('../lib/functions')
-const axios = require('axios')
 
 cmd({
-
     pattern: "menu",
-
-    react: "🛸",
-
-    alias: ["panel","commands"],
-
-    desc: "Get bot\'s command list.",
-
-    category: "main",
-
-    use: '.menu',
-
-    filename: __filename
-
+    alias: ["list"],
+    desc: "menu the bot",
+    react: "📜",
+    category: "main"
 },
+async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply }) => {
+    try {
+        let desc = `*👋 Hello ${pushname}*
 
-async(conn, mek, m,{from, l, quoted, body, isCmd, umarmd, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
-
-try{
-let madeMenu = `╭━━━━∙⋆⋅⋆∙━ ─┉─ • ─┉─⊷
-
-*⇆ ʜɪɪ ᴍʏ ᴅᴇᴀʀ ғʀɪᴇɴᴅ ⇆*
-
-     *${pushname}*
-     
-╰━━━━∙⋆⋅⋆∙━ ─┉─ • ─┉─⊷
-┏━━━━━━━━━━━━━━━━━━━━━━━━━━
-      *ᴡᴇʟᴄᴏᴍᴇ ᴛᴏ sɪʟᴇɴᴛ-sᴏʙx-ᴍᴅ ғᴜʟʟ ᴄᴏᴍᴍᴀɴᴅ ʟɪsᴛ*
-┗━━━━━━━━━━━━━━━━━━━━━━━━━━
-
-*ᴄʀᴇᴀᴛᴇᴅ ʙʏ sɪʟᴇɴᴛ ʟᴏᴠᴇʀ⁴³²👨🏻‍💻*
-
-*┌─〈 ${config.BOT_NAME} 〉─◆*
 *│╭─────────────···▸*
 *┴│▸*
 *❖│▸* *ʀᴜɴᴛɪᴍᴇ* : ${runtime(process.uptime())}
 *❖│▸* *ᴍᴏᴅᴇ* : *[${config.MODE}]*
 *❖│▸* *ᴘʀᴇғɪx* : *[${config.PREFIX}]*
 *❖│▸* *ʀᴀᴍ ᴜsᴇ* : ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}MB / ${Math.round(require('os').totalmem / 1024 / 1024)}MB
-*❖│▸* *ɴᴀᴍᴇ ʙᴏᴛ* : *❖sɪʟᴇɴᴛ-sᴏʙx-ᴍᴅ❖*
-*❖│▸* *ᴄʀᴇᴀᴛᴏʀ* : *➺ѕเℓεɳƭ_ℓσѵε૨࿐*
-*❖│▸* *ᴠᴇʀsɪᴏɴs* : *ᴠ.2.0.0*
+*❖│▸* *ɴᴀᴍᴇ ʙᴏᴛ* : *❖QUEEN-ISHU❖*
+*❖│▸* *ᴄʀᴇᴀᴛᴏʀ* : *➺LAKSIDU NIMSARA࿐*
+*❖│▸* *ᴠᴇʀsɪᴏɴs* : *ᴠ.1.0.0*
 *❖│▸* *ᴍᴇɴᴜ ᴄᴍᴅ* : *ᴍᴇɴᴜ ʟɪsᴛ*
 *┬│▸*
 *│╰────────────···▸▸*
-*└──────────────···▸*
 *♡︎•━━━━━━☻︎━━━━━━•♡︎*
-
-*╭────❒⁠⁠⁠⁠* *📥 DOWNLOADER-CMD 📥* *❒⁠⁠⁠⁠* 
-*┋* *.ғʙ <ᴜʀʟ>*
-*┋* *.ɪɴꜱᴛᴀ <ᴜʀʟ>*
-*┋* *.ᴠɪᴅᴇᴏ <ᴜʀʟ>*
-*┋* *.ɢᴅʀɪᴠᴇ <ᴜʀʟ>*
-*┋* *.ᴛᴡɪᴛᴛᴇʀ <ᴜʀʟ>*
-*┋* *.ᴛᴛ<ᴜʀʟ>*
-*┋* *.ᴍᴇᴅɪᴀғɪʀᴇ <ᴜʀʟ>*
-*┋* *.ꜱᴏɴɢ <ϙᴜᴇʀʏ>*
-*┋* *.ᴘʟᴀʏ <ᴜʀʟ>*
-*┋* *.ᴠɪᴅᴇᴏ <ϙᴜᴇʀʏ>*
-*┋* *.ᴠɪᴅᴇᴏ <ᴜʀʟ>*
-*┋* *.ɪᴍɢ <ϙᴜᴇʀʏ>*
-*┋* *.ᴀᴘᴋ <ɴᴀᴍᴇ>*
-*┋* *.ᴅᴀʀᴀᴍᴀ <ᴛɪᴛᴛʟᴇ>*
-*┋* *.ᴘʟᴀʏ2 <ᴛɪᴛᴛʟᴇ>*
-*┕───────────────────❒*
-
-*╭────❒⁠⁠⁠⁠* *🔎 SEARCH-CMD 🔍* *❒⁠⁠⁠⁠* 
-*┋* *.ʏᴛꜱ  <ᴛᴇxᴛ>*
-*┋* *.ʟᴏʟɪ <ᴛᴇxᴛ>*
-*┋* *.ᴍᴏᴠɪᴇ <ᴛᴇxᴛ>*
-*┋* *.ɪᴍɢ <ᴛᴇxᴛ>*
-*┋* *.ᴡᴇᴀᴛʜᴇʀ <ᴄɪᴛʏ>
-*┕───────────────────❒*
-
-*╭────❒⁠⁠⁠⁠* *🧠 AI-CMD 🧠* *❒⁠⁠⁠⁠* 
-*┋* *.ɢᴘᴛ <ᴛᴇxᴛ>*
-*┋* *.ᴀɪ <ᴛᴇxᴛ>*
-*┕───────────────────❒*
-
-*╭────❒⁠⁠⁠⁠* *👨‍💻 OWNER-CMD 👨‍💻* *❒⁠⁠⁠⁠* 
-*┋* *.ᴜᴘᴅᴀᴛᴇᴄᴍᴅ*
-*┋* *.ᴏᴡɴᴇʀ*
-*┋* *.ʀᴇᴘᴏ*
-*┋* *.ꜱʏꜱᴛᴇᴍ*
-*┋* *.ꜱᴛᴀᴛᴜꜱ*
-*┋* *.ʙʟᴏᴄᴋ*
-*┋* *.ᴜɴʙʟᴏᴄᴋ*
-*┋* *.sʜᴜᴛᴅᴏᴡɴ*
-*┋* *.ᴄʟᴇᴀʀᴄʜᴀᴛs*
-*┋* *.sᴇᴛᴘᴘ*
-*┋* *.ʙʀᴏᴀᴅᴄᴀsᴛ*
-*┋* *.ᴊɪᴅ*
-*┋* *.ɢᴊɪᴅ*
-*┋* *.ʀᴇꜱᴛᴀʀᴛ*
-*┕───────────────────❒*
-
-*╭────❒⁠⁠⁠⁠* *👥 GROUP-CMD 👥* *❒⁠⁠⁠⁠* 
-*┋* *.ʀᴇᴍᴏᴠᴇ <ʀᴇᴘʟʏ ғᴏʀ ᴅᴇʟᴇᴛᴇ sᴍs>*
-*┋* *.ᴅᴇʟᴇᴛᴇ <ʀᴇᴘʟʏ ғᴏʀ ᴅᴇʟᴇᴛᴇ sᴍs>*
-*┋* *.ᴀᴅᴅ*
-*┋* *.ᴋɪᴄᴋ*
-*┋* *.sᴇᴛɢᴏᴏᴅʙʏᴇ <ᴛᴇxᴛ>*
-*┋* *.sᴇᴛᴡᴇʟᴄᴏᴍᴇ <ᴛᴇxᴛ>*
-*┋* *.ᴘʀᴏᴍᴏᴛᴇ*
-*┋* *.ᴅᴇᴍᴏᴛᴇ*
-*┋* *.ᴛᴀɢᴀʟʟ*
-*┋* *.ɢᴇᴛᴘɪᴄ*
-*┋* *.ɪɴᴠɪᴛᴇ*
-*┋* *.ʀᴇᴠᴏᴋᴇ*
-*┋* *.ᴊᴏɪɴʀᴇǫᴜᴇsᴛs*
-*┋* *.ᴀʟʟʀᴇǫ*
-*┋* *.ᴍᴜᴛᴇ*
-*┋* *.ᴜɴᴍᴜᴛᴇ*
-*┋* *.ʟᴏᴄᴋɢᴄ*
-*┋* *.ᴜɴʟᴏᴄᴋɢᴄ*
-*┋* *.ʟᴇᴀᴠᴇ*
-*┋* *.ᴜᴘᴅᴀᴛᴇɢɴᴀᴍᴇ*
-*┋* *.ᴜᴘᴅᴀᴛᴇɢᴅᴇsᴄ*
-*┋* *.ᴊᴏɪɴ*
-*┋* *.ʜɪᴅᴇᴛᴀɢ*
-*┋* *.ɢɪɴғᴏ*
-*┋* *.ᴅɪsᴀᴘᴘᴇᴀʀ ᴏɴ*
-*┋* *.ᴅɪsᴀᴘᴘᴇᴀʀ ᴏғғ*
-*┋* *.ᴅɪsᴀᴘᴘᴇᴀʀ 7ᴅ 24ʜ 90ᴅ*
-*┋* *.sᴇɴᴅᴅᴍ*
-*┕───────────────────❒*
-
-*╭────❒⁠⁠⁠⁠* *📃 INFO-CMD 📃* *❒⁠⁠⁠⁠* 
-*┋* *.ᴍᴇɴᴜ*
-*┋* *.ᴍᴇɴᴜ2*
-*┋* *.ᴍᴇɴᴜ3*
-*┋* *.ᴀʙᴏᴜᴛ*
-*┋* *.sᴄʀɪᴘᴛ*
-*┋* *.ʀᴇᴘᴏ*
-*┋* *.ᴀʟɪᴠᴇ*
-*┋* *.ʙᴏᴛɪɴꜰᴏ*
-*┋* *.ꜱᴛᴀᴛᴜꜱ*
-*┋* *.ꜱᴜᴘᴘᴏʀᴛ*
-*┋* *.ᴘɪɴɢ*
-*┋* *.ᴘɪɴɢ2*
-*┋* *.ꜱʏꜱᴛᴇᴍ*
-*┕───────────────────❒*
-
-*╭────❒⁠⁠⁠⁠* *🎡 CONVERTER-CMD 🎡* *❒⁠⁠⁠⁠* 
-*┋* *.sᴛɪᴄᴋᴇʀ*
-*┕───────────────────❒*
-
-*╭────❒⁠⁠⁠⁠* *⛱️ RANDOM-CMD ⛱️* *❒⁠⁠⁠⁠* 
-*┋* *.ᴋɪɴɢ*
-*┋* *.ᴅᴏɢ*
-*┋* *.ᴀɴɪᴍᴇ*
-*┋* *.ᴀɴɪᴍᴇɢɪʀʟ*
-*┋* *.ᴀɴɪᴍᴇɢɪʀʟ1*
-*┋* *.ᴀɴɪᴍᴇɢɪʀʟ2*
-*┋* *.ᴀɴɪᴍᴇɢɪʀʟ3*
-*┋* *.ᴀɴɪᴍᴇɢɪʀʟ4*
-*┋* *.ᴀɴɪᴍᴇɢɪʀʟ5*
-*┕───────────────────❒*
-
-*╭────❒⁠⁠⁠⁠* *🏜️ WALLPAPERS-CMD 🏜️* *❒⁠⁠⁠⁠* 
-*┋* *.ɪᴍɢ*
-*┕───────────────────❒*
-
-*╭────❒⁠⁠⁠⁠* *🌐 OTHER-CMD 🌐* *❒⁠⁠⁠⁠* 
-*┋* *.ᴛʀᴛ*
-*┋* *.ᴊᴏᴋᴇ*
-*┋* *.ᴍᴏᴠɪᴇ*
-*┋* *.ꜰᴀᴄᴛ*
-*┋* *.ɢɪᴛʜᴜʙꜱᴛᴀʟᴋ*
-*┋* *.ɢᴘᴀꜱꜱ*
-*┋* *.ʜᴀᴄᴋ*
-*┋* *.ǫᴜᴏᴛᴇ*
-*┋* *.ꜱʀᴇᴘᴏ*
-*┋* *.ᴅᴇꜰɪɴᴇ*
-*┕───────────────────❒*
-
+*╭╼╼╼╼╼╼╼╼╼╼*
+*├ 1 • OWNER*
+*├ 2 • CONVERT*
+*├ 3 • AI*
+*├ 4 • SEARCH*
+*├ 5 • DOWNLOAD*
+*├ 6 • MAIN*
+*├ 7 • GROUP*
+*├ 8 • FUN*
+*├ 9 • TOOLS*
+*├ 10 • OTHER*
+*╰╼╼╼╼╼╼╼╼╼╼*
 *❒⁠⁠⁠⁠▭▬▭▬▭▬▭▬▭▬▭▬▭❒*⁠⁠⁠⁠
+*🌟 Reply with the Number you want to select*
 
-┏━━━━━━━━━━━━━━━━━━━
-*https://whatsapp.com/channel/0029VaHO5B0G3R3cWkZN970s*
-┗━━━━━━━━━━━━━━━━━━━
+*ISHU MD CREATE BY LAKSIDU NIMSARAッ*`;
+
+        const vv = await conn.sendMessage(from, { image: { url: "https://i.imgur.com/wzC6N6h.jpeg"}, caption: desc }, { quoted: mek });
+
+        conn.ev.on('messages.upsert', async (msgUpdate) => {
+            const msg = msgUpdate.messages[0];
+            if (!msg.message || !msg.message.extendedTextMessage) return;
+
+            const selectedOption = msg.message.extendedTextMessage.text.trim();
+
+            if (msg.message.extendedTextMessage.contextInfo && msg.message.extendedTextMessage.contextInfo.stanzaId === vv.key.id) {
+                switch (selectedOption) {
+                    case '1':
+                        reply(`*💜◈╾──────OWNER COMMAND LIST──────╼◈💜*
+
+╭────────●●►
+│ 🧡 *restart* 
+╰────────────────────●●►
+
+⭓ *Total Commands List OWNER: 1*
+
+*©ISHU MD CREATE BY LAKSIDU NIMSARAッ*`);
+                        break;
+                    case '2':               
+                        repl (`💜*◈╾──────CONVERT COMMAND LIST──────╼◈💜*
+
+╭────────●●►
+│ 🧡 *convert* 
+╰────────────────────●●►
+
+⭓ *Total Commands List CONVERT: 1*
+
+*©ISHU MD CREATE BY LAKSIDU NIMSARAッ*`);
+                        break;
+                    case '3':               
+                        reply(`💜*◈╾──────AI COMMAND LIST──────╼◈💜*
+
+╭────────●●►
+│ 🧡 *ai* 
+╰────────────────────●●►
+
+⭓ *Total Commands List AI: 1*
+
+*©ISHU MD CREATE BY LAKSIDU NIMSARAッ*`);
+                        break;
+                    case '4':               
+                        reply(`*💜◈╾──────SEARCH COMMAND LIST──────╼◈💜*
+
+╭────────●●►
+│ 🧡 *yts* 
+╰────────────────────●●►
+╭────────●●►
+│ 🧡 *srepo* 
+╰────────────────────●●►
+
+⭓ *Total Commands List SEARCH: 2*
+
+*ISHU MD CREATE BY LAKSIDU NIMSARAッ*`);
+                        break;
+                    case '5':               
+                        reply(`*💜◈╾──────DOWNLOAD COMMAND LIST──────╼◈💜*
+
+╭────────●●►
+│ 🧡 *apk* 
+╰────────────────────●●►
+╭────────●●►
+│ 🧡 *twitter* 
+╰────────────────────●●►
+╭────────●●►
+│ 🧡 *gdrive* 
+╰────────────────────●●►
+╭─🧡───────●●►
+│ 🧡 *mediafire* 
+╰────────────────────●●►
+╭────────●●►
+│ 🧡 *fb* 
+╰────────────────────●●►
+╭────────●●►
+│ 🧡 *ig* 
+╰────────────────────●●►
+╭────────●●►
+│ 🧡 *movie* 
+╰────────────────────●●►
+╭────────●●►
+│ 🧡 *song* 
+╰────────────────────●●►
+╭────────●●►
+│ 🧡 *video* 
+╰────────────────────●●►
+╭────────●●►
+│ 🧡 *play/yt* 
+╰────────────────────●●►
+╭────────●●►
+│ 🧡 *song2* 
+╰────────────────────●●►
+╭────────●●►
+│ 🧡 *video2* 
+╰────────────────────●●►
+╭────────●●►
+│ 🧡 *tiktok* 
+╰────────────────────●●►
+╭────────●●►
+│ 🧡 *img* 
+╰────────────────────●●►
+
+⭓ *Total Commands List DOWNLOAD: 14*
+
+*©ISHU MD CREATE BY LAKSIDU NIMSARAッ*`);
+                        break;
+                    case '6':               
+                        reply(`*💜◈╾──────MAIN COMMAND LIST──────╼◈💜*
+
+╭────────●●►
+│ 🧡 *alive* 
+╰────────────────────●●►
+╭────────●●►
+│ 🧡 *about* 
+╰────────────────────●●►
+╭────────●●►
+│ 🧡 *menu* 
+╰────────────────────●●►
+╭────────●●►
+│ 🧡 *allmenu* 
+╰────────────────────●●►
+╭────────●●►
+│ 🧡 *support* 
+╰────────────────────●●►
+╭────────●●►
+│ 🧡 *system* 
+╰────────────────────●●►
+╭────────●●►
+│ 🧡 *ping* 
+╰────────────────────●●►
+╭────────●●►
+│ 🧡 *runtime* 
+╰────────────────────●●►
+
+⭓ *Total Commands List MAIN: 8*
+
+*©ISHU MD CREATE BY LAKSIDU NIMSARAッ*`);
+                        break;
+                    case '7':               
+                        reply(`*💜◈╾──────GROUP COMMAND LIST──────╼◈💜*
+
+╭────────●●►
+│ 🧡 *promote* 
+╰────────────────────●●►
+╭────────●●►
+│ 🧡 *demote* 
+╰────────────────────●●►
+╭────────●●►
+│ 🧡 *kick* 
+╰────────────────────●●►
+╭────────●●►
+│ 🧡 *add* 
+╰────────────────────●●►
+╭────────●●►
+│ 🧡 *admins* 
+╰────────────────────●●►
+╭────────●●►
+│ 🧡 *tagall* 
+╰────────────────────●●►
+╭────────●●►
+│ 🧡 *getpic* 
+╰────────────────────●●►
+╭────────●●►
+│ 🧡 *setwelcome* 
+╰────────────────────●●►
+╭────────●●►
+│ 🧡 *setgoodbye* 
+╰────────────────────●●►
+╭────────●●►
+│ 🧡 *admins* 
+╰────────────────────●●►
+╭────────●●►
+│ 🧡 *gname* 
+╰────────────────────●●►
+
+⭓ *Total Commands List GROUP: 11*
+
+*ISHU MD CREATE BY LAKSIDU NIMSARAッ*`);
+                       break;
+                    case '8':               
+                        reply(`*💜◈╾──────FUN COMMAND LIST──────╼◈💜*
+
+╭────────●●►
+│ 🧡 *dog* 
+╰────────────────────●●►
+╭────────●●►
+│ 🧡 *fact* 
+╰────────────────────●●►
+╭────────●●►
+│ 🧡 *hack* 
+╰────────────────────●●►
+╭────────●●►
+│ 🧡 *quote* 
+╰────────────────────●●►
+
+⭓ *Total Commands List FUN: 4*
+
+*ISHU MD CREATE BY LAKSIDU NIMSARAッ*`);
+
+                        break;
+                    case '10':               
+                        reply(`*💜◈╾──────OTHER COMMAND LIST──────╼◈💜*
+
+╭────────●●►
+│ 🧡 *githubstalk* 
+╰────────────────────●●►
+╭────────●●►
+│ 🧡 *trt* 
+╰────────────────────●●►
+╭────────●●►
+│ 🧡 *weather* 
+╰────────────────────●●►
+
+⭓ *Total Commands List OTHER: 3*
+
+*ISHU MD CREATE BY LAKSIDU NIMSARAッ*`);
 
 
-> *POWERED BY SILENTLOVER432*
-╘✦•·········•••••••••············•✦ 
-`
+                        break;
+                    default:
+                        reply("Invalid option. Please select a valid option🔴");
+                }
 
-await conn.sendMessage(from,{image:{url:config.ALIVE_IMG},caption:madeMenu},{quoted:mek})
+            }
+        });
 
-}catch(e){
-console.log(e)
-reply(`${e}`)
-}
-})
+    } catch (e) {
+        console.error(e);
+        await conn.sendMessage(from, { react: { text: '❌', key: mek.key } })
+        reply('An error occurred while processing your request.');
+    }
+});
